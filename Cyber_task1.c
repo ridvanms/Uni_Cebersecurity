490ecd438ddf2ed597dbf80493fac8a1b53acd9e
Ridvan <ridvans@abv.bg>
2025-02-23 17:54:44 +0200

    `Refactored is_prime function and main function with minor formatting changes`

---
+++ Cyber_task1.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+bool is_prime(int num){
+  if(num < 2){
+    return false;
+  }
+  if(num == 2){
+    return true;
+  }
+  if(num % 2 == 0){
+    return false;
+  }
+  int end = (int)sqrt((double)num);
+  for(int i = 3; i <= end; i += 2){
+    if(num % i == 0){
+      return false;
+    }
+  }
+  return true;
+}
+
+int main(){
+  int num;
+  printf("Enter a number: ");
+  if(scanf("%d",&num) != 1){
+    fprintf(stderr, "Invalid input Please enter an integer.\n");
+    return EXIT_FAILURE;
+    
+  }
+  if(is_prime(num)){
+    printf("%d is a prime number.\n", num);
+  }else {
+    printf("%d is not a prime number.\n", num);
+  }
+  return EXIT_SUCCESS;
+}
