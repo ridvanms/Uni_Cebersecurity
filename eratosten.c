68a612db3897d67bbe2571f58f7a2a7c648ca2d5
ridvanms <ridvans@abv.bg>
2025-04-01 13:03:33 +0300

    Refactor Sieve of Eratosthenes implementation for improved readability and consistency

--- eratosten.c
+++ eratosten.c
@@ -1,35 +1,34 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 bool* eratosten(int n)
 {
- bool* seive = (bool*)malloc(sizeof(bool)*(n+1));
- memset(seive,false,sizeof(bool)*(n+1));
- for(int i=2;i<=n;i++)
- {
-    if(seive[i]==false)
+    bool* seive = (bool*)malloc(sizeof(bool)*(n+1));
+    memset(seive,false,sizeof(bool)*(n+1));
+
+    for(int i=2;i<=n;i++)
     {
-        for(int j = i+i;j<=n;j+=i)
+        if(seive[i]==false)
         {
-            seive[j]=true;
+            for(int j = i+i; j <= n;j+=i){
+                seive[j] = true;
+            }
         }
     }
- }
- return seive;
+    return seive;
 }
 
-
-void print_numbers(bool * seive,int n)
+void printNumbers(bool* seive,int n)
 {
-  for(int i = 2;i<=n;i++)
-  {
-    if(!seive[i])
+    for(int i = 2;i <= n;i++)
     {
-        printf("%d ",i);
+        if(!seive[i])
+        {
+            printf("%d ",i);
+        }
     }
-  }
 }
 
 int main()
@@ -37,7 +36,7 @@ int main()
     int n;
     scanf("%d",&n);
 
-    bool *seive = eratosten(n);
-    print_numbers(seive,n);
+    bool* seive = eratosten(n);
+    printNumbers(seive,n);
     return EXIT_SUCCESS;
 }
