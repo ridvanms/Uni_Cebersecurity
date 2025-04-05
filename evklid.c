d0db3b139b7c4adc85235b29797d5210853824a5
ridvanms <ridvans@abv.bg>
2025-04-05 16:01:11 +0300

    Refactor code structure for improved readability and maintainability

---
+++ evklid.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int NOD(int a, int b)
+{
+    while(a != b)
+    {
+        if(a > b)
+        {
+            a = a-b;
+        }
+        else
+        {
+            b = b-a;
+        }
+    }
+    return a;
+}
+
+
+int main()
+{
+    int a = 0,b=0;
+    printf("Insert a: ");
+    scanf("%d",&a);
+    printf("Insert b: ");
+    scanf("%d",&b);
+
+    int result = NOD(a,b);
+    printf("The NOD of %d and %d = %d",a,b,result);
+
+    return EXIT_SUCCESS;
+}
